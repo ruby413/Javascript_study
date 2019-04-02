c554edb4c638b92ef0845046b1bf8a2e0783093e
LeeJuyeon <ruby413@hanmail.net>
2019-04-02 17:17:10 +0900

    codesquade first day and second day

---
+++ codesquad/algorithm/programmers/programmers_add_quiz.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+using namespace std;
+
+long long solution(int a, int b) {
+    
+    long long resultnum = 0;
+    if (a < b) {
+        for ( int i=a; i<=b; i++ ){
+            resultnum= resultnum+i;
+        };
+        return resultnum;
+    }else if(a > b){
+        for ( int i=b; i<=a; i++ ){
+            resultnum= resultnum+i;
+        };
+        return resultnum;
+    }else{
+        return a;
+    }
+}
+
+int main() {
+    
+    int firstnum;
+    int lastnum;
+    long long resultnum = 0;
+    
+    cin >> firstnum >> lastnum;
+    cout << solution(firstnum,lastnum) << endl;
+
+    
+    
+    return 0;
+    
+    
+}
+
+
