04df137c9b9f95bde9ec65a34d914cb96ad2c66a
LeeJuyeon <ruby413@hanmail.net>
2019-05-22 14:50:04 +0900

    최대공약수와 최소공배수

---
+++ algorithm/programmers/12940.js
@@ -0,0 +1,9 @@
+const solution = (n, m) => {
+    var answer = [];
+    answer = [smallMath(n,m) , (n*m)/smallMath(n,m)]
+    return answer
+}
+
+const smallMath = (r, m) => {
+    return r % m === 0 ? m : smallMath(m, r % m)
+}
