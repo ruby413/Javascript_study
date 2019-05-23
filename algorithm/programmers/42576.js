e4fb5d81aea57eaed7b4e5ed53e0132975ecac5f
LeeJuyeon <ruby413@hanmail.net>
2019-05-23 14:44:50 +0900

    readdir

--- algorithm/programmers/42576.js
+++ algorithm/programmers/42576.js
@@ -1,12 +1,12 @@
 
 const solution = (participant, completion) => {
-    let partPeople = participant.sort()
+    let partiPeople = participant.sort()
     let completePeople = completion.sort();
-    for (let i = 0; i < participant.length; i++) {
-        if (partPeople[i] !== completePeople[i]) {
-            return partPeople[i] 
+    for ( let i = 0; i < partiPeople.length; i++ ) {
+        if ( partiPeople[i] !== completePeople[i] ) {
+            return partiPeople[i] 
         }
     }
 }
 
-console.log(solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"]))
+console.log( solution(["mislav", "stanko", "mislav", "ana"],["stanko", "ana", "mislav"]) )
