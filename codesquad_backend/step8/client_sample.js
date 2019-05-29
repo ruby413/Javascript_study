d3980771ac66da5c2eee86a943a5229fa4e533fc
LeeJuyeon <ruby413@hanmail.net>
2019-05-29 23:46:22 +0900

    다중 채팅 창 완성

--- codesquad_backend/step8/client_sample.js
+++ codesquad_backend/step8/client_sample.js
@@ -1,112 +1,24 @@
-// // Import net module.
-// var net = require('net');
-
-// // This function create and return a net.Socket object to represent TCP client.
-// function getConn(connName){
-
-//     var option = {
-//         host:'localhost',
-//         port: 9999
-//     }
-
-//     // Create TCP client.
-//     var client = net.createConnection(option, function () {
-//         console.log('Connection name : ' + connName);
-//         console.log('Connection local address : ' + client.localAddress + ":" + client.localPort);
-//         console.log('Connection remote address : ' + client.remoteAddress + ":" + client.remotePort);
-//     });
-
-//     client.setTimeout(1000);
-//     client.setEncoding('utf8');
-
-//     // When receive server send back data.
-//     client.on('data', function (data) {
-//         console.log('Server return data : ' + data);
-//     });
-
-//     // When connection disconnected.
-//     client.on('end',function () {
-//         console.log('Client socket disconnect. ');
-//     });
-
-//     client.on('timeout', function () {
-//         console.log('Client connection timeout. ');
-//     });
-
-//     client.on('error', function (err) {
-//         console.error(JSON.stringify(err));
-//     });
-
-//     return client;
-// }
-
-// // Create a java client socket.
-// var javaClient = getConn('Java');
-
-// // Create node client socket.
-// var nodeClient = getConn('Node');
-
-// javaClient.write('Java is best programming language. ');
-
-// nodeClient.write('Node is more better than java. ');
-
-// var net = require('net');
-// var client = new net.Socket();
-// client.connect(52275, '127.0.0.1', function() {
-// 	console.log('Connected');
-// 	client.write('Hello, server! Love, Client.');
-// });
-// var i = 0;
-// client.on('data', function(data) {
-// 	console.log('Received: ' + data);
-// 	i++;
-// 	if(i==2)
-// 		client.destroy(); 
-// });
-// client.on('close', function() {
-// 	console.log('Connection closed');
-// });
-
-/**
- * Created by AvILoS on 6/16/15.
- */
-// var net = require('net');
-// // 5000번 포트로 TCP서버 접속 요청
-// var client = net.connect({port:5000}, function(){
-//      console.log('클라이언트가 서버에 연결되었습니다.;;;;)');
-
-//     // 서버측으로 'hello'담아서 데이터 전송
-//     client.write('hello', function(){
-//         //client.end();
-//     });
-// });
-
-// // 서버측으로부터 데이터 수신시 이벤트 잡아 출력
-// client.on('data', function(data){
-//     console.log('from server:'+data);
-//     //client.end();
-// });
-
-// client.on('end', function(){
-//     console.log('클라이언트가 서버에서 연결 해제되었습니다.');
-// });
-
-
-
 /**
  * Created by AvILoS on 6/16/15.
  */
 var net = require('net');
-    
-process.stdin.resume();         // 표준입력처리(입력받을수 있도록 변경)
+var readline = require('readline')
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 var client = net.connect(
-        {host:'localhost', port:5000},
-        function(){
-            console.log(client.localPort + ' is connection!' );
-        });
+    {host:'localhost', port:5000},
+    function(){
+        console.log(client.localPort + ' is connection!' );
+        rl.prompt();
+});
+//process.stdin.resume();         // 표준입력처리(입력받을수 있도록 변경)
+
 
 client.on('data', function(data){       // 서버로부터 데이터 수신시 콘솔에 출력
-        console.log("someone : " + data.toString());
+    console.log("someone : " + data.toString());
+    rl.prompt();
 });
 
 client.on('end', function(data){
@@ -117,13 +29,26 @@ client.on('close', function(){
     console.log('close client....');
 });
 
-// 사용자가 콘솔에서 텍스트를 입력하였을 경우 write함수로 서버에 전송
-process.stdin.on('data', function(data){
-    if(data.toString().trim().toLowerCase() == 'quit'){
+rl.on('line', function(line){
+    if (line == 'exit'){
         console.log('request disconnect');
-        client.write(data);
-        process.stdin.end();
+        client.write(line);
+        rl.close()
     }else{
-        client.write(data);
+        client.write(line);
     }
+    rl.prompt()
+});
+rl.on('close', function() {
+    process.exit()
 })
+// // 사용자가 콘솔에서 텍스트를 입력하였을 경우 write함수로 서버에 전송
+// process.stdin.on('data', function(data){
+//     if(data.toString().trim().toLowerCase() == 'quit'){
+//         console.log('request disconnect');
+//         client.write(data);
+//         process.stdin.end();
+//     }else{
+//         client.write(data);
+//     }
+// })
--- codesquad_backend/step8/server_sample.js
+++ codesquad_backend/step8/server_sample.js
@@ -1,89 +1,3 @@
-// // Import net module.
-// var net = require('net');
-
-// // Create and return a net.Server object, the function will be invoked when client connect to this server.
-// var server = net.createServer(function(client) {
-
-//     console.log('Client connect. Client local address : ' + client.localAddress + ':' + client.localPort + '. client remote address : ' + client.remoteAddress + ':' + client.remotePort);
-
-//     client.setEncoding('utf-8');
-
-//     client.setTimeout(1000);
-
-//     // When receive client data.
-//     client.on('data', function (data) {
-
-//         // Print received client data and length.
-//         console.log('Receive client send data : ' + data + ', data size : ' + client.bytesRead);
-
-//         // Server send data back to client use client net.Socket object.
-//         client.end('Server received data : ' + data + ', send back to client data size : ' + client.bytesWritten);
-//     });
-
-//     // When client send data complete.
-//     client.on('end', function () {
-//         console.log('Client disconnect.');
-
-//         // Get current connections count.
-//         server.getConnections(function (err, count) {
-//             if(!err)
-//             {
-//                 // Print current connection count in server console.
-//                 console.log("There are %d connections now. ", count);
-//             }else
-//             {
-//                 console.error(JSON.stringify(err));
-//             }
-
-//         });
-//     });
-
-//     // When client timeout.
-//     client.on('timeout', function () {
-//         console.log('Client request time out. ');
-//     })
-// });
-
-// // Make the server a TCP server listening on port 9999.
-// server.listen(9999, function () {
-
-//     // Get server address info.
-//     var serverInfo = server.address();
-
-//     var serverInfoJson = JSON.stringify(serverInfo);
-
-//     console.log('TCP server listen on address : ' + serverInfoJson);
-
-//     server.on('close', function () {
-//         console.log('TCP server socket is closed.');
-//     });
-
-//     server.on('error', function (error) {
-//         console.error(JSON.stringify(error));
-//     });
-
-// });
-
-
-
-
-// var net = require('net');
-// var textChunk = '';
-// var server = net.createServer(function(socket) {
-// 	socket.write('Echo server\r\n');
-// 	socket.on('data', function(data){
-// 		console.log(data);
-// 		textChunk = data.toString('utf8');
-// 		console.log(textChunk);
-// 		socket.write(textChunk);
-// 	});
-// });
-// server.listen(52275, '127.0.0.1');
-
-
-/**
- * Created by AvILoS on 6/16/15.
- */
 var net = require('net');
 var port = 5000;
 var server = net.createServer(function(client){
@@ -97,16 +11,16 @@ var server = net.createServer(function(client){
         clientSockets.push(socket);     // 사용자가 접속시 마다 소켓을 저장(향후 텍스트 전송시 이 배열만 조회하면 된다.
         // 사용자가 접속시마다 데이터를 전송해오면 접속한 클라이언트의 소켓을
         // clientSockets배열에서 비교하여 모두 전송처리
-        console.log(clientSockets.length)
-        if(clientSockets.length <= 2){
+        console.log(clientSockets.length + "번째 접속자입니다.")
+        // if(clientSockets.length <= 2){
             socket.on('data', function(data){
+            console.log(socket._peername.port +" : "+ data.toString())
                 if(data.toString().trim().toLowerCase() == 'quit'){
                     socket.write('>>>> disconnect client....');
                     return socket.end();
                 }
                 clientSockets.forEach(function(otherSocket){
                     if(otherSocket!=socket){
-                        console.log(otherSocket._peername.port +" : "+ data.toString())
                         otherSocket.write(data);
                     }
                 });
@@ -119,10 +33,6 @@ var server = net.createServer(function(client){
                     clientSockets.splice(index, 1); // 리스트에서 제거
                 }
             })
-        }else{
-            console.log("두명 넘어감")
-        }
-
 
     });
 
