777ed2d5cad60d73bcdd44f506e95f349de8b292
LeeJuyeon <ruby413@hanmail.net>
2019-06-14 10:44:52 +0900

    최댓값과 최솟값

--- algorithm/programmers/12939.js
+++ algorithm/programmers/12939.js
@@ -2,5 +2,5 @@ const solution = (s) => {
     let strArr = s.split(' ')
     let numStr = []
     strArr.forEach((el)=>{ numStr.push(Number(el))})
-    return Math.min(...numStr) +" "+ Math.max(...numStr);
+    return Math.min(...numStr) +" "+ Math.max(...numStr); 
 }
