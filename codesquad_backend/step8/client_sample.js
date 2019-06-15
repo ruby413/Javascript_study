d23891c51206a0d19ad7676145e1070f9a08c652
LeeJuyeon <ruby413@hanmail.net>
2019-06-15 23:15:13 +0900

    d

--- codesquad_backend/step8/client_sample.js
+++ codesquad_backend/step8/client_sample.js
@@ -1,32 +1,20 @@
-/**
- * Created by AvILoS on 6/16/15.
- */
-var net = require('net');
-var readline = require('readline')
-var rl = readline.createInterface({
+const net = require('net');
+const readline = require('readline')
+let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-var client = net.connect(
+const client = net.connect(
     {host:'localhost', port:5000},
     function(){
         console.log(client.localPort + ' is connection!' );
         rl.prompt();
 });
-//process.stdin.resume();         // 표준입력처리(입력받을수 있도록 변경)
+let answer = ""
 
-
-client.on('data', function(data){       // 서버로부터 데이터 수신시 콘솔에 출력
+client.on('data', function(data){   
     console.log("someone : " + data.toString());
     rl.prompt();
-});
-
-client.on('end', function(data){
-    console.log(data.toString());
-});
-
-client.on('close', function(){
-    console.log('close client....');
 });
 
 rl.on('line', function(line){
@@ -35,20 +23,8 @@ rl.on('line', function(line){
         client.write(line);
         rl.close()
     }else{
+        answer = line
         client.write(line);
     }
     rl.prompt()
 });
-rl.on('close', function() {
-    process.exit()
-})
-// // 사용자가 콘솔에서 텍스트를 입력하였을 경우 write함수로 서버에 전송
-// process.stdin.on('data', function(data){
-//     if(data.toString().trim().toLowerCase() == 'quit'){
-//         console.log('request disconnect');
-//         client.write(data);
-//         process.stdin.end();
-//     }else{
-//         client.write(data);
-//     }
-// })
