9381287464a04ec0f08e723bc2dd736887d8ee63
Ruby Lee <ruby413@hanmail.com>
2019-07-19 17:17:58 +0900

    create : express study

--- node/expressExam/server.js
+++ node/expressExam/server.js
@@ -8,7 +8,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-
+  
 var server = app.listen(3000, function(){
  console.log("Express server has started on port 3000")
 });
