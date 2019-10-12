c9fd0e28f5df5731004b2a1b34d65e7100080aa5
Ruby Lee <ruby413@hanmail.net>
2019-10-12 15:13:44 +0900

    타겟넘버 (dfs/bfs) - 다시 풀어봐야 함

---
+++ algorithm/programmers/43165.js
@@ -0,0 +1,14 @@
+const solution = (numbers, target) => {
+    let stack = [];
+    let resultNumber = 0;
+    let answer = 0;
+    
+    numbers.forEach((el)=>{
+        resultNumber + = el;
+    })
+    if(resultNumber === target){
+        answer + = 1;
+    }
+}
+
+// try again!
